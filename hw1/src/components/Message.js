ffe768486f0de0d4ee922e54492f760e53a453a5
SA <artka2016@gmail.com>
2024-12-21 18:15:47 +0500

    hw1

---
+++ hw1/src/components/Message.js
@@ -0,0 +1,12 @@
+import "./stylemessage.css";
+
+function Message({title, text}) {
+    return (
+        <div className="message">
+            <p className="message__title">{title}</p>
+            <p className="message__text">{text}</p>
+        </div>
+    )
+}
+
+export default Message;
